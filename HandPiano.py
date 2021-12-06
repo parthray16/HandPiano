f40b3769c383e549b318a962af6d4eec93d2a156
Gaurav Joshi <49354547+gjoshi2424@users.noreply.github.com>
2021-12-05 19:40:43 -0800

    Fixed playsound/hand&thumb detection

--- HandPiano.py
+++ HandPiano.py
@@ -3,6 +3,13 @@ import mediapipe as mp
 import time
 from playsound import playsound
 
+def check_thumb(hand, prev_x, cur_x):
+    if hand == "Left" and prev_x- 35 > cur_x:
+        return True
+    if hand == "Right" and prev_x + 35 < cur_x:
+        return True
+    return False
+
 def main():
     cap = cv2.VideoCapture(0)
 
@@ -17,6 +24,7 @@ def main():
     cTime = 0
     finger_points = {3, 4, 7, 8, 11, 12, 15, 16, 19, 20}
     finger_dict = {4: "thumb", 8: "index finger", 12: "middle finger", 16: "ring finger", 20: "pinky finger"}
+    pressed = {"4Left": 0, "4Right": 0, "8Left": 0, "8Right" : 0, "12Left": 0, "12Right": 0,"16Left": 0, "16Right": 0, "20Left": 0, "20Right": 0}
     while True:
         success, img = cap.read()
         # when using an inverted camera
@@ -24,9 +32,12 @@ def main():
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = hands.process(imgRGB)
         if results.multi_hand_landmarks:
-            for handLms in results.multi_hand_landmarks:
+            #print(results.multi_hand_landmarks)
+            for idx, handLms in enumerate(results.multi_hand_landmarks):
+                hand = results.multi_handedness[idx].classification[0].label
                 prev_id = -2
                 prev_y = None
+                prev_x = None
                 for id, lm in enumerate(handLms.landmark):
                     #This will draw finger_tips and the points right below it
                     if id in finger_points:
@@ -37,15 +48,28 @@ def main():
                         #This set of if statements will check if a finger has been put down
                         # and it will print which finger (more testing needed)
                         if id == prev_id + 1:
-                            if cy > prev_y:
-                                print(f"{id=}")
-                                if results.multi_handedness[0].classification[0].label == 'Left':
-                                    print("The " + "left " + finger_dict[id] + " was put down")
-                                else:
-                                    print("The " + "right " + finger_dict[id] + " was put down")
-                                playsound(f"Notes/key{id}.mp3", block=False)
+                            #hand = results.multi_handedness[0].classification[0].label
+                            thumb = False
+                            if id == 4:
+                                thumb = check_thumb(hand, prev_x, cx)
+                            p_index = str(id) + hand
+                            
+                            if cy > prev_y or thumb:
+                                if pressed[p_index] == 0:
+                                    #print(p_index)
+                                    pressed[p_index] = 1
+                                    #print(f"{id=}")
+                                    if hand == 'Left':
+                                        print("The " + "left " + finger_dict[id] + " was put down")
+                                        playsound(f"Notes/key{id-1}.mp3", block=False)
+                                    else:
+                                        print("The " + "right " + finger_dict[id] + " was put down")
+                                        playsound(f"Notes/key{id}.mp3", block=False)
+                            else:
+                                pressed[p_index] = 0
                         prev_id = id
                         prev_y = cy
+                        prev_x = cx
                 # connect the landmarks
                 #mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
